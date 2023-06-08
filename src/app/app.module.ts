697b03ee36f0c96ebfc9a4ab83ea9b0cbde3d79a
muchokiesther <muchokiesther8@gmail.com>
2023-06-08 14:09:17 +0300

    Logins and Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SignUpComponent,
+    LoginComponent,
+    LandingPageComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
